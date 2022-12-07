bb6d9670cbe23b6cffe5939d9047b44891f9d796
FeeinCof <tuan.letrong.dev@gmail.com>
2022-12-07 18:13:03 +0700

    update process

---
+++ src/components/OverViewContainer/OverViewContainer.js
@@ -0,0 +1,9 @@
+function OverViewContainer (props) {
+    return (
+        <>
+            <div>hello</div>
+        </>
+    )
+}
+
+export default OverViewContainer;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,6 +1,7 @@
 import react, { Component } from 'react';
 import './Home.css';
-
+import OverViewContainer from '../../components/OverViewContainer/OverViewContainer';
+import hackingIcon from '../../assets/images/hacking_icon.svg';
 class Home extends Component {
     constructor (props) {
         super();
@@ -10,7 +11,21 @@ class Home extends Component {
         return (
             <>
                 <div className='Home'>
-                    <h1>Home</h1>
+                    <div className='container-fluid bg-orange'>
+                        <div className='container overview-component'>
+                            <div className='overview-left'>
+                                <div className='overview-title'>
+                                    <div className='overview-title-image'>
+                                        <img src={hackingIcon} />
+                                    </div>
+                                    <h1>Hacking the Product Management Interview</h1>
+                                </div>
+                            </div>
+                            <div className='overview-right'>
+                                <h1>right</h1>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </>
         )
